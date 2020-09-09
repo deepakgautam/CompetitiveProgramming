463b89f9f40f2449faa772b738cc64c5ce8218cd
Deepak Gautam <degautam@microsoft.com>
2020-09-09 23:42:37 +0530

    09/09/2020 - problems - [M- Rabbits in Forest ]

--- leetcode/781. Rabbits in Forest.cpp
+++ leetcode/781. Rabbits in Forest.cpp
@@ -1,3 +1,15 @@
+/*
+problem link - https://leetcode.com/problems/rabbits-in-forest/
+
+Deepak gautam
+leetcode - https://leetcode.com/deepakgautam270795/
+codechef - https://www.codechef.com/users/algorithmist2
+codeforces - https://codeforces.com/profile/gautam27
+Spoj - https://www.spoj.com/users/nexus_d/
+hackerearth- https://www.hackerearth.com/@deepak.gautam.127648 
+hackerrank - https://www.hackerrank.com/deepakgautam2701
+*/
+
 class Solution {
  public:
   int numRabbits(vector<int> &answers) {
